637b7faf4f0ffefc8ed1f55e843cb74b09181d00
Ira <izubareva874@gmail.com>
2026-06-04 17:13:55 +0300

    first commit

---
+++ CoreMVC_DatabaseFirst/Data/PubsContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreMVC_DatabaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVC_DatabaseFirst.Data;
+
+public partial class PubsContext : DbContext
+{
+    public PubsContext()
+    {
+    }
+
+    public PubsContext(DbContextOptions<PubsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
+
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.AuId).HasName("UPKCL_auidind");
+
+            entity.Property(e => e.Phone)
+                .HasDefaultValue("UNKNOWN")
+                .IsFixedLength();
+            entity.Property(e => e.State).IsFixedLength();
+            entity.Property(e => e.Zip).IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CoreMVC_DatabaseFirst/Models/Author.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVC_DatabaseFirst.Models;
+
+[Table("authors")]
+[Index("AuLname", "AuFname", Name = "aunmind")]
+public partial class Author
+{
+    [Key]
+    [Column("au_id")]
+    [StringLength(11)]
+    [Unicode(false)]
+    public string AuId { get; set; } = null!;
+
+    [Column("au_lname")]
+    [StringLength(40)]
+    [Unicode(false)]
+    public string AuLname { get; set; } = null!;
+
+    [Column("au_fname")]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string AuFname { get; set; } = null!;
+
+    [Column("phone")]
+    [StringLength(12)]
+    [Unicode(false)]
+    public string Phone { get; set; } = null!;
+
+    [Column("address")]
+    [StringLength(40)]
+    [Unicode(false)]
+    public string? Address { get; set; }
+
+    [Column("city")]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string? City { get; set; }
+
+    [Column("state")]
+    [StringLength(2)]
+    [Unicode(false)]
+    public string? State { get; set; }
+
+    [Column("zip")]
+    [StringLength(5)]
+    [Unicode(false)]
+    public string? Zip { get; set; }
+
+    [Column("contract")]
+    public bool Contract { get; set; }
+}
