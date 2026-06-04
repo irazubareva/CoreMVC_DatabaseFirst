785fdf492ad487991d54c9ce957bba000628a121
Ira <izubareva874@gmail.com>
2026-06-04 19:43:18 +0300

    Person add

---
+++ CoreMVC_DatabaseFirst/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace CoreMVC_DatabaseFirst.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Age { get; set; }
+        public string Weight { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
